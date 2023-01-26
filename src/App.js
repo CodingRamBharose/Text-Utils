d76949c44de4096d30d82992c20d8973476dc213
Ram Avtar <ramavtarsharma85507@gmail.com>
2023-01-26 11:27:46 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-    {/* <Navbar title="TextUtils" /> */}
     <div className="container my-3">
       <TextForm heading="Enter Your Text Here:"/>
     </div>
